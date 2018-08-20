404b693d338cfef368f6e5bec35478e11acba7f3
MarcoKruger <2908@ctucareer.co.za>
2018-08-20 09:11:14 +0200

    Initial commit

---
+++ WebStore/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Products> Products = new List<Products>()
+            {
+
+            };
+            return View(Products);
+        }
+        public ActionResult Edit(int? selects)
+        {
+            var id = Request.QueryString["id"];
+
+            var getId = Request[id];// Can also be the string name for the input name
+
+            return View();
+        }
+    }
+}
